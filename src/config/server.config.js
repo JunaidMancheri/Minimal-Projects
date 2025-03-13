7417cfa84856ee4580477914d41c20e488f7ee09
Junaid <junaidofficialnow@gmail.com>
2025-03-13 17:05:06 +0530

    feat: add .env.example

--- src/config/server.config.js
+++ src/config/server.config.js
@@ -1,5 +1,5 @@
 const app = require('../app');
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 const server = app.listen(port, ()=>{
   console.info(`Server started at the port ${process.env.PORT}`);
